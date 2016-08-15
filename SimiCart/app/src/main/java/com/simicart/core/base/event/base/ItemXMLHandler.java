b5e41264ac9c1a5742c2f344c85ebdf2cb32fa9f
nhatvm <minhnhat.vu@trueplus.vn>
2016-08-15 16:37:31 +0700

    upgrade event mechanism

---
+++ SimiCart/app/src/main/java/com/simicart/core/base/event/base/ItemXMLHandler.java
@@ -0,0 +1,96 @@
+package com.simicart.core.base.event.base;
+
+/**
+ * Created by frank on 8/15/16.
+ */
+
+import com.simicart.core.common.Utils;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+
+public class ItemXMLHandler extends DefaultHandler {
+
+    private Boolean currentElement = false;
+    private String currentValue = "";
+    private String tags = "event";
+    private String currentSKU;
+    private String currentFullName;
+    ArrayList<String> mListSKU;
+
+
+    public ItemXMLHandler(ArrayList<String> listSKU) {
+        this.mListSKU = listSKU;
+    }
+
+
+    public void startElement(String uri, String localName, String qName,
+                             Attributes attributes) throws SAXException {
+
+        currentElement = true;
+        currentValue = "";
+
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName)
+            throws SAXException {
+
+        currentElement = false;
+
+        if (localName.equalsIgnoreCase("fullname")) {
+            currentFullName = currentValue;
+            currentValue = "";
+
+        } else if (localName.equalsIgnoreCase("sku")) {
+            currentSKU = currentValue;
+            currentValue = "";
+        } else if (localName.equalsIgnoreCase(this.tags)) {
+            if (Utils.validateString(currentSKU) && checkSKU(currentSKU)) {
+                try {
+                    Class<?> change = Class.forName(currentFullName);
+                    change.getConstructor().newInstance();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void characters(char[] ch, int start, int length)
+            throws SAXException {
+
+        if (currentElement) {
+            currentValue = currentValue + new String(ch, start, length);
+        }
+
+    }
+
+
+    private boolean checkSKU(String sku) {
+        if (null != mListSKU && mListSKU.size() > 0) {
+            for (int i = 0; i < mListSKU.size(); i++) {
+                String currentSKU = mListSKU.get(i);
+                if (sku.equals(currentSKU)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
---
+++ SimiCart/app/src/main/java/com/simicart/core/base/event/base/ReadXML.java
@@ -0,0 +1,68 @@
+package com.simicart.core.base.event.base;
+
+/**
+ * Created by frank on 8/15/16.
+ */
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+public class ReadXML {
+
+    Context context;
+    String tags = "event";
+    ArrayList<String> mListSKU;
+
+    public ReadXML(Context context, ArrayList<String> listSKU) {
+        this.context = context;
+        this.mListSKU = listSKU;
+    }
+
+    public void read() {
+        String[] files = this.readXml();
+        for (int i = 0; i < files.length; i++) {
+            this.getItemMaster(files[i]);
+        }
+    }
+
+    public String[] readXml() {
+        AssetManager assetManager = context.getAssets();
+        try {
+            String[] files = assetManager.list("plugins");
+            return files;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void getItemMaster(String filename) {
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        SAXParser sp;
+        XMLReader xr;
+        AssetManager assetManager = context.getAssets();
+
+        try {
+            InputStream inputStream = assetManager.open("plugins/" + filename);
+            InputSource inStream = new InputSource(inputStream);
+            sp = spf.newSAXParser();
+            xr = sp.getXMLReader();
+            ItemXMLHandler myXMLHandler = new ItemXMLHandler(mListSKU);
+            xr.setContentHandler(myXMLHandler);
+            xr.parse(inStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
