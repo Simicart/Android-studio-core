64be76c008458a8dcc0337722863117c8a2d6bb2
nhatvm <minhnhat.vu@trueplus.vn>
2016-08-16 14:22:03 +0700

    remove Plugins

--- SimiCart/app/src/main/java/com/simicart/plugins/barcode/ConstantBarcode.java
+++
@@ -1,7 +0,0 @@
-package com.simicart.plugins.barcode;
-
-public class ConstantBarcode {
-
-    public static final String SCAN_NOW = "Scan QR Barcode";
-    public static final String QR_BAR_CODE = "Scan Now";
-}
